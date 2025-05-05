7f301476953df91d585daea8a4ecbb400dc9919f
GuZzKiR <112087032+GuZzKiR@users.noreply.github.com>
2025-05-06 00:04:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
